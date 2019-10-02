c068aa67790dd842f72a51a86eee5f0da6bee524
Tyler Casperson <tylercasperson@gmail.com>
2019-10-01 20:19:15 -0600

    changed the firebase call

--- app.js
+++ app.js
@@ -11,28 +11,17 @@ https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItems
 
 //Firebase setup...still need to change storage
 
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/7.0.0/firebase-app.js"></script>
+const firebaseConfig = {
+  apiKey: "AIzaSyBucOADTvEUKByNXdujg0Xz01JGOrw94S4",
+  authDomain: "comicsearch-baf00.firebaseapp.com",
+  databaseURL: "https://comicsearch-baf00.firebaseio.com",
+  projectId: "comicsearch-baf00",
+  storageBucket: "",
+  messagingSenderId: "569479066676",
+  appId: "1:569479066676:web:8328e59cd4e99f11430661",
+  measurementId: "G-EEQ0N3BQTZ"
+};
 
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/7.0.0/firebase-analytics.js"></script>
 
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyBucOADTvEUKByNXdujg0Xz01JGOrw94S4",
-    authDomain: "comicsearch-baf00.firebaseapp.com",
-    databaseURL: "https://comicsearch-baf00.firebaseio.com",
-    projectId: "comicsearch-baf00",
-    storageBucket: "",
-    messagingSenderId: "569479066676",
-    appId: "1:569479066676:web:8328e59cd4e99f11430661",
-    measurementId: "G-EEQ0N3BQTZ"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-</script>
 
 
