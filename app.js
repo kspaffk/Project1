9e97467ab656c9a63b567a4e350b47c13b794502
Tyler Casperson <tylercasperson@gmail.com>
2019-10-05 11:03:06 -0600

    Added the high, low, open, close from alpha vantage

--- app.js
+++ app.js
@@ -1,56 +1,46 @@
+
 // Alpha Vantage API:
 
   var APIkey = "CTSV8BKGR6BEKQ3F";
-  var ticker = "MSFT"
-
-//Time Series
-  var timeseriesURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + ticker + "&interval=5min&apikey=" + APIkey;
-
-  $.ajax({
-    url: timeseriesURL,
-    method: "GET"
-  })
-  .then(function(response){
-    console.log(response)
-  });
-
-
-        // Transfer content to HTML
-//        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-  //      $(".wind").text("Wind Speed: " + response.wind.speed);
-    //    $(".humidity").text("Humidity: " + response.main.humidity);
-//        $(".temp").text("Temperature (F) " + response.main.temp);
-
-        // Log the data in the console as well
-//        console.log("Wind Speed: " + response.wind.speed);
-//        console.log("Humidity: " + response.main.humidity);
-//        console.log("Temperature (F): " + response.main.temp);
-//      });
-
-
+  var ticker = "MSFT";
+  var stockdate = "2019-10-04";
+  
 //Daily Adjusted
-  var DailyAdjustedURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + ticker + "&apikey=" + APIkey
+var DailyAdjustedURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + ticker + "&apikey=" + APIkey
 
-  $.ajax({
-    url: DailyAdjustedURL,
-    method: "GET"
-  })
-  .then(function(response){
- //   $(".high").text("High: " + response.high)
+$.ajax({
+  url: DailyAdjustedURL,
+  method: "GET"
+})
+.then(function(response){
+  console.log(stockdate)
+  console.log(response["Time Series (Daily)"][stockdate]["2. high"])
+  console.log(response["Time Series (Daily)"][stockdate]["3. low"])
+  console.log(response["Time Series (Daily)"][stockdate]["1. open"])
+  console.log(response["Time Series (Daily)"][stockdate]["4. close"])
 
-    console.log(response)
-  });
+  var div = $("<div>").addClass("row");
+  div.append(getPriceCard(stockdate,response["Time Series (Daily)"][stockdate]["2. high"],response["Time Series (Daily)"][stockdate]["3. low"],response["Time Series (Daily)"][stockdate]["1. open"],response["Time Series (Daily)"][stockdate]["4. close"]))
+  $("body").append(div)
+});
 
 //Weekly Adjusted
-  var WeeklyAdjustedURL = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=" + ticker + "&apikey=" + APIkey
+var WeeklyAdjustedURL = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=" + ticker + "&apikey=" + APIkey
 
-  $.ajax({
-    url: WeeklyAdjustedURL,
-    method: "GET"
-  })
-  .then(function(response){
-    console.log(response)
-  });
+$.ajax({
+  url: WeeklyAdjustedURL,
+  method: "GET"
+})
+.then(function(response){
+  console.log(stockdate)
+  console.log(response["Weekly Adjusted Time Series"][stockdate]["2. high"])
+  console.log(response["Weekly Adjusted Time Series"][stockdate]["3. low"])
+  console.log(response["Weekly Adjusted Time Series"][stockdate]["1. open"])
+  console.log(response["Weekly Adjusted Time Series"][stockdate]["4. close"])
+
+
+
+});
 
 //Monthly Adjusted
   var MonthlyAdjustedURL = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=" + ticker + "&apikey=" + APIkey
@@ -60,6 +50,11 @@
     method: "Get"
   })
   .then(function(response){
-    console.log(response)
-  });
+    console.log(stockdate)
+    console.log(response["Monthly Adjusted Time Series"][stockdate]["2. high"])
+    console.log(response["Monthly Adjusted Time Series"][stockdate]["3. low"])
+    console.log(response["Monthly Adjusted Time Series"][stockdate]["1. open"])
+    console.log(response["Monthly Adjusted Time Series"][stockdate]["4. close"])
 
+
+  });
