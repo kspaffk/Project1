5d864393875a8dde7ade4ad8c39337c9a09cdde8
kent <51271998+kspaffk@users.noreply.github.com>
2019-10-04 19:33:42 -0600

    remove old stuff from comic site and add function to create price cards

--- assets/js/materialize.js
+++ assets/js/materialize.js
@@ -6,31 +6,59 @@
 //     // Or with jQuery
 
 $(document).ready(function(){
-    // https://materializecss.com/carousel.html - documentation
-    $('.carousel').carousel();
-    // https://materializecss.com/sidenav.html - doumentation 
-    // slide out side nav
-    $('.sidenav').sidenav();
-
-
-    // function to create the carousel - it takes an array of links and pictures for the carousel
-    function createCarousel(linkArray, picArray) {
-        var caro = $("<div>").addClass("carousel");
-        var caroItem = $("<a>").addClass("carousel-item");
-
-        // for each item add the classes
-        linkArray.forEach(element, i => {
-            if (picArray[i]) {
-                caroItem.attr({
-                    href: element,
-                    src: picArray[i]
-                });
-            }
-            caro.append(caroItem);
-        });
-        // return the carousel jquery object
-        return caro;
+    // function to get HTML for price cards
+    function getPriceCard(date, high, low, open, close) {
+        // create the container for the card
+        var container = $("<div>").addClass("col s12 m3");
+        // create the card display area
+        var mainCard = $("<div>").addClass("main-card card cyan lighten-1");
+        // specify the card content area
+        var cardContent = $("<div>").addClass("card-content white-text");
+        // add the date as the title
+        var cardTitle = $("<span>").addClass("card-title").text(date);
+        // add the closing price to the card
+        var closePrice = $("<span>").addClass("close-price").text(close);
+        // add another card container for the opening price
+        var openCard = $("<div>").addClass("col s12 m4");
+        // create open card display area
+        var openCardContent = $("<div>").addClass("card cyan lighten-4");
+        // add opening price
+        var openPrice = $("<span>").addClass("other-prices black-text").text(open);
+        // add opening title
+        var openTitle = $("<span>").addClass("other-price-titles black-text").text("Open");
+        // add another card container for the high price
+        var highCard = $("<div>").addClass("col s12 m4");
+        // create high card display area
+        var highCardContent = $("<div>").addClass("card cyan lighten-4");
+        // add highing price
+        var highPrice = $("<span>").addClass("other-prices black-text").text(high);
+        // add highing title
+        var highTitle = $("<span>").addClass("other-price-titles black-text").text("High");
+        // add another card container for the low price
+        var lowCard = $("<div>").addClass("col s12 m4");
+        // create low card display area
+        var lowCardContent = $("<div>").addClass("card cyan lighten-4");
+        // add lowing price
+        var lowPrice = $("<span>").addClass("other-prices black-text").text(low);
+        // add lowing title
+        var lowTitle = $("<span>").addClass("other-price-titles black-text").text("Low");
+        
+        // append items to the open card
+        openCardContent.append(openPrice, openTitle);
+        openCard.append(openCardContent);
+        // append items to the high card
+        highCardContent.append(highPrice, highTitle);
+        highCard.append(highCardContent);
+        // append items to the low card
+        lowCardContent.append(lowPrice, lowTitle);
+        lowCard.append(lowCardContent);
+        // append items to the main card
+        cardContent.append(cardTitle, closePrice, openCard, highCard, lowCard);
+        mainCard.append(cardContent);
+        container.append(mainCard);
+        // return the container object
+        return container;
     }
 
-
+    $(".kent").append(getPriceCard("2019-02-19", 15.65, 15.15, 15.31, 15.42));
 });
