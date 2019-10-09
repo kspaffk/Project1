0a24d5f4be863fdcada8bfb0996ba9e6e5fb1667
kent <51271998+kspaffk@users.noreply.github.com>
2019-10-08 23:57:32 -0600

    add search bar

--- assets/js/materialize.js
+++ assets/js/materialize.js
@@ -14,32 +14,26 @@ function getPriceCard(date, high, low, open, close, priceTimeframe) {
     }
 
     // create the card display area
-    var mainCard = $("<div>").addClass("main-card card cyan lighten-1");
+    var mainCard = $("<div>").addClass("main-card card  cyan lighten-1");
     // specify the card content area
     var cardContent = $("<div>").addClass("card-content white-text");
     // add the date as the title
-    var cardTitle = $("<span>").addClass("card-title").text(date);
+    var cardTitle = $("<span>").addClass("card-title").text(date + " - " + priceTimeframe + " adjusted price");
     // add the closing price to the card
     var closePrice = $("<span>").addClass("close-price").text(close);
-    // add another card container for the opening price
-    var openCard = $("<div>").addClass("col s12 s4 l4");
     // create open card display area
     var openCardContent = $("<div>").addClass("card cyan lighten-4");
     // add opening price
     var openPrice = $("<span>").addClass("other-prices black-text").text(open);
     // add opening title
     var openTitle = $("<span>").addClass("other-price-titles black-text").text("Open");
-    // add another card container for the high price
-    var highCard = $("<div>").addClass("col s12 s4 l4");
-    // create high card display area
+    // // create high card display area
     var highCardContent = $("<div>").addClass("card cyan lighten-4");
     // add highing price
     var highPrice = $("<span>").addClass("other-prices black-text").text(high);
     // add highing title
     var highTitle = $("<span>").addClass("other-price-titles black-text").text("High");
-    // add another card container for the low price
-    var lowCard = $("<div>").addClass("col s12 s4 l4");
-    // create low card display area
+    // // create low card display area
     var lowCardContent = $("<div>").addClass("card cyan lighten-4");
     // add lowing price
     var lowPrice = $("<span>").addClass("other-prices black-text").text(low);
@@ -48,15 +42,12 @@ function getPriceCard(date, high, low, open, close, priceTimeframe) {
 
     // append items to the open card
     openCardContent.append(openPrice, openTitle);
-    openCard.append(openCardContent);
-    // append items to the high card
+    // append items to the high card 
     highCardContent.append(highPrice, highTitle);
-    highCard.append(highCardContent);
     // append items to the low card
     lowCardContent.append(lowPrice, lowTitle);
-    lowCard.append(lowCardContent);
     // append items to the main card
-    cardContent.append(cardTitle, closePrice, openCard, highCard, lowCard);
+    cardContent.append(cardTitle, closePrice, openCardContent, highCardContent, lowCardContent);
     mainCard.append(cardContent);
     priceContainer.append(mainCard);
     // return the container object
@@ -64,7 +55,7 @@ function getPriceCard(date, high, low, open, close, priceTimeframe) {
 }
 
 
-//examples
+//example
 // $(".news-row").append(getNewsCards("Avengers Save the Planet", "The Avengers finally figured out time travel and went back in town to kill Thanos before he used the stones.", "https://google.com", "https://cdn.vox-cdn.com/thumbor/QjYjrRLVMwCgjZSfE7JjC7URQns=/0x300:3733x2254/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/19253276/1040241490.jpg.jpg"));
 function getNewsCards(newsTitle, newsDescription, newsURL, newsURLtoImg) {
     // create the container for the card
@@ -85,7 +76,7 @@ function getNewsCards(newsTitle, newsDescription, newsURL, newsURLtoImg) {
         href: newsURL,
         target: "_blank"
     });
-    var linkIcon = $("<i>").addClass("material-icons").text("Go");
+    var linkIcon = $("<i>").addClass("material-icons").text("arrow_forward_ios");
     // card content which has an article description
     var newsDescDiv = $("<div>").addClass("card-content");
     // paragraph for news description
@@ -108,36 +99,46 @@ $(document).ready(function () {
 
     $("#submit-button").on("click", function (event) {
         event.preventDefault();
-        var userSearch = $("#search").val().trim();
-        $(".main-content").empty();
-        var tickerDiv = $("<div>").addClass("ticker-div");
-        var stockDiv = $("<div>").addClass("stock-div row");
-        var newsDiv = $("<div>").addClass("news-div row");
-
-        tickerDiv.html("<h1>" + userSearch + "</h1>");
-        $(".main-content").append(tickerDiv, stockDiv, newsDiv);
-        stockinfo(userSearch)
-        newsFunction(userSearch);
-
-        createSearchBarHeader();
+        populatePageData();
     });
 });
 
-function createSearchBarHeader() {
-    var div = $("<div>").addClass("header-search");
-    var form = $("<form>");
-    var searchBar = $("<input>").attr({
-        id: "h-search.input",
-        type: "text",
-        placeholder: "Stock Symbol"
-    });
-    var goButton = $("<button>").addClass("h-submit-button").attr({
-        id: "submit-button",
-        type: "submit"
-    });
+function populatePageData() {
+    var userSearch = $(".search-input").val().trim();
+    $(".main-content").empty();
+    var tickerDiv = $("<div>").addClass("ticker-div");
+    var stockDiv = $("<div>").addClass("stock-div row");
+    var newsDiv = $("<div>").addClass("news-div row");
 
-    form.append(searchBar, goButton);
-    div.append(form);
-    $("header").append(div);
+    tickerDiv.html("<h1>" + userSearch + "</h1>");
+    $(".main-content").append(tickerDiv, stockDiv, newsDiv);
+    stockinfo(userSearch)
+    newsFunction(userSearch);
+
+    createSearchBarHeader();
+}
+
+function createSearchBarHeader() {
+    var form = $("<form>").addClass("col s4").attr("id", "header-search");
+    var row = $("<div>").addClass('row');
+    var div = $("<div>").addClass("input-field");
+    var searchIcon = $("<i>").addClass("material-icons prefix").text("search");
+    var searchField = $("<input>").addClass("search-input").attr({
+        id: "icon_search",
+        type: "text",
+    });
+    var searchLabel = $("<label>").addClass("h-label").attr("for", "icon-search");
+    var helper = $("<span>").addClass("helper-text").text("Enter Stock Symbol");
+
+    div.append(searchIcon, searchField, searchLabel, helper);
+    row.append(div);
+    form.append(row);
+    $(".header-row").append(form);
+
+    $("form").on("submit", function (event) {
+        event.preventDefault();
+        populatePageData();
+        $("#header-search").remove();
+    });
 };
 
